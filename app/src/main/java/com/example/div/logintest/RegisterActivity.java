a94eeda570752d62df43899e77cb0fa9d746babc
iamthedival <bandiv2011@gmail.com>
2016-05-26 20:14:41 -0400

    Activity Setup - You can now click the sign up text view to visit the Register Activity

--- app/src/main/java/com/example/div/logintest/RegisterActivity.java
+++ app/src/main/java/com/example/div/logintest/RegisterActivity.java
@@ -2,6 +2,8 @@ package com.example.div.logintest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        final EditText etName = (EditText)findViewById(R.id.etName);
+        final EditText etUsername = (EditText)findViewById(R.id.etUsername);
+        final EditText etEmail = (EditText)findViewById(R.id.etEmail);
+        final EditText etPassword = (EditText)findViewById(R.id.etPassword);
+        final Button btnLogin = (Button) findViewById(R.id.btnLogin);
+
     }
 }
